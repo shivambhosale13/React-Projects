c544841291a83764d7b6cfa0308e69ad3ef59df1
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-18 14:38:43 +0530

    added styling to about section

--- Digital-Business-Card/src/components/about.jsx
+++ Digital-Business-Card/src/components/about.jsx
@@ -1,8 +1,8 @@
 export default function About(){
   return(
     <div className="about">
-      <h1>About-Header</h1>
-      <p>About-info</p>
+      <h1>About</h1>
+      <p>I'm a software developer who enjoys building things for the web and learning new technologies. I'm currently focused on improving my skills in JavaScript, React, and AI development. I enjoy turning ideas into projects and continuously challenging myself to become a better developer.</p>
     </div>
   ) 
 }
