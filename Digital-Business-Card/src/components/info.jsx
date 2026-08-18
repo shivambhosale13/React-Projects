cbfb543488a129d149c805535ba60a80b3936895
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-18 11:55:46 +0530

    setup code for info and main

---
+++ Digital-Business-Card/src/components/info.jsx
@@ -0,0 +1,16 @@
+export default function Info(){
+  return (
+    <div className="info-container">
+      <h1>Photo Goes here</h1>
+      <p>Name Goes here</p>
+      <button className="Email-button">
+        <>icon goes here </>
+        Email
+      </button>
+      <button>
+        <>icon goes here </>
+        LinkedIn
+      </button>
+    </div>
+  )
+}
---
+++ Digital-Business-Card/src/main.jsx
@@ -0,0 +1,10 @@
+import {createRoot} from 'react-dom/client';
+import Info from './components/info';
+
+const root = createRoot(document.querySelector('#root'));
+
+
+root.render(
+  <Info/>
+)
+
