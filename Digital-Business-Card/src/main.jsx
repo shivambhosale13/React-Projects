c2ce239d05169aee93d52fd9fd850c3d29863fd6
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-18 12:02:10 +0530

    set-up code for about page

--- Digital-Business-Card/src/main.jsx
+++ Digital-Business-Card/src/main.jsx
@@ -1,10 +1,16 @@
 import {createRoot} from 'react-dom/client';
 import Info from './components/info';
+import About from './components/about'
 
 const root = createRoot(document.querySelector('#root'));
 
 
 root.render(
-  <Info/>
+  <>
+    <Info />
+    <About />
+  </>
+  
+
 )
 
