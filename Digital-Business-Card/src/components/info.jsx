b4c6b854ef750cf7cbbbdc26d558b94f201987cd
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-18 13:21:29 +0530

    added styling to the info section

--- Digital-Business-Card/src/components/info.jsx
+++ Digital-Business-Card/src/components/info.jsx
@@ -1,16 +1,31 @@
-export default function Info(){
-  return (
-    <div className="info-container">
-      <h1>Photo Goes here</h1>
-      <p>Name Goes here</p>
-      <button className="Email-button">
-        <>icon goes here </>
-        Email
-      </button>
-      <button>
-        <>icon goes here </>
-        LinkedIn
-      </button>
-    </div>
-  )
+export default function Info() {
+    return (
+        <div className="info-container">
+
+            <img
+                src="/profile.jpeg"
+                alt="Profile"
+                className="profile-photo"
+            />
+
+            <div className="info">
+                <h1>Shivam Bhosale</h1>
+                <h2>Software Developer</h2>
+                <h3>github.com/shivambhosale13</h3>
+            </div>
+
+            <div className="social-media">
+                <button className="Email-button">
+                    <i className="fa-solid fa-envelope"></i>
+                    Email
+                </button>
+
+                <button className="LinkedIn-button">
+                    <i className="fa-brands fa-linkedin"></i>
+                    LinkedIn
+                </button>
+            </div>
+
+        </div>
+    );
 }
