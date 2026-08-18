5d1b4acab9c801565150222b72ac6903c8d62bd9
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-18 15:42:53 +0530

    added styles to footer

--- Digital-Business-Card/src/components/footer.jsx
+++ Digital-Business-Card/src/components/footer.jsx
@@ -1,7 +1,10 @@
 export default function Footer(){
   return(
     <footer>
-      <p>Footer Goes Here</p>
+      <i class="fa-brands fa-square-x-twitter"></i>
+      <i class="fa-brands fa-square-facebook"></i>
+      <i class="fa-brands fa-square-instagram"></i>
+      <i class="fa-brands fa-square-github"></i>
     </footer>
   ) 
 }
