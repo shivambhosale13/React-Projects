44c5cd038119c05f0475ba1fb79dbf261cff0bf7
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-19 16:38:58 +0530

    added destructuring

--- Contact-Book/src/Contact.jsx
+++ Contact-Book/src/Contact.jsx
@@ -1,25 +1,39 @@
-export default function Contact(){
-  return (
-    <article className="contact-card">
-        <img 
-            src="./images/mr-whiskerson.png"
-            alt="Photo of Mr. Whiskerson"
-        />
-        <h3>Mr. Whiskerson</h3>
-        <div className="info-group">
-            <img 
-                src="./images/phone-icon.png" 
-                alt="phone icon" 
+/**
+ * Challenge: fix the bug, now that we've 
+ * destructured the props object
+ */
+export default function Contact({img, name, phone, email}) {
+    return (
+        <article className="contact-card">
+            <img
+                src={img}
+                alt="Photo of Mr. Whiskerson"
             />
-            <p>(212) 555-1234</p>
-        </div>
-        <div className="info-group">
-            <img 
-                src="./images/mail-icon.png" 
-                alt="mail icon"
-            />
-            <p>mr.whiskaz@catnap.meow</p>
-        </div>
-    </article>
-  )
+            <h3>{name}</h3>
+            <div className="info-group">
+                <img
+                    src="./images/phone-icon.png"
+                    alt="phone icon"
+                />
+                <p>{phone}</p>
+            </div>
+            <div className="info-group">
+                <img
+                    src="./images/mail-icon.png"
+                    alt="mail icon"
+                />
+                <p>{email}</p>
+            </div>
+        </article>
+    )
 }
+
+const person = {
+    img: "./images/mr-whiskerson.png",
+    name: "Mr. Whiskerson",
+    phone: "(800) 555-1234",
+    email: "mr.whiskaz@catnap.meow"
+}
+
+const {img, name} = person
+console.log(img)
