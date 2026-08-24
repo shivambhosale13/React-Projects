1128e1f878d6ec114fb72c7a4a9ed08fc05c5e34
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-24 12:54:27 +0530

    refactor Main component to use form action for ingredient submission

--- Chef-Claude/src/components/Main.jsx
+++ Chef-Claude/src/components/Main.jsx
@@ -1,34 +1,26 @@
 import React from "react"
+
 export default function Main() {
 
-    /**
-     * Challenge: Update our app so that when the user enters a
-     * new ingredient and submits the form, it adds that new
-     * ingredient to our list!
-     */
-
-    const [ingredients , setIngredients]= React.useState(["Chicken", "Oregano", "Tomatoes"])
+    const [ingredients, setIngredients] = React.useState([])
 
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
+    
+    /**
+     * Challenge: use form action instead of onSubmit to
+     * handle the data from the form
+     */
 
-    function handleSubmit(event) {
-        /**
-         * Like before, don't worry about this FormData stuff yet.
-         * Just use the newIngredient below to help you finish the
-         * challenge.
-         */
-
-        setIngredients(prevIngredients => [...prevIngredients , newIngredient])
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function handleSubmit(formData) {
         const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
     return (
         <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <form action = {handleSubmit} className="add-ingredient-form">
                 <input
                     type="text"
                     placeholder="e.g. oregano"
