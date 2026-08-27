affaa506a5495100591f8f8606f3306340467fa0
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-27 17:25:03 +0530

    refactor Main component to extract IngredientsList for improved code organization and readability

--- Chef-Claude/src/components/Main.jsx
+++ Chef-Claude/src/components/Main.jsx
@@ -1,40 +1,16 @@
 import React from "react"
 import ClaudeRecipe from "./ClaudeRecipe"
-export default function Main() {
-    /**
-     * Challenge: clean up our code!
-     * Let's make a couple new components to make things a
-     * little cleaner. (Notice: I'm not suggesting what we
-     * have now is bad or wrong. I'm mostly finding an excuse
-     * to get in some hands-on practice 🙂)
-     * 
-     * 1. Move the entire recipe <section> into its own
-     *    ClaudeRecipe component
-     * 2. Move the list of ingredients <section> into its
-     *    own IngredientsList component.
-     * 
-     * While you're considering how to structure things, consider
-     * where state is, think about if it makes sense or not to
-     * move it somewhere else, how you'll communicate between
-     * the parent/child components, etc.
-     * 
-     * The app should function as it currently does when you're
-     * done, so there will likely be some extra work to be done
-     * beyond what I've listed above.
-     */
+import IngredientsList from "./IngredientsList"
 
+export default function Main() {
     const [ingredients, setIngredients] = React.useState(
         ["all the main spices", "pasta", "ground beef", "tomato paste"]
     )
     const [recipeShown, setRecipeShown] = React.useState(false)
-    
+
     function toggleRecipeShown() {
         setRecipeShown(prevShown => !prevShown)
     }
-
-    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
 
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
@@ -52,19 +28,14 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            
-            {ingredients.length > 0 && <section>
-                <h2>Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-                {ingredients.length > 3 && <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button onClick={toggleRecipeShown}>Get a recipe</button>
-                </div>}
-            </section>}
-            
+
+            {ingredients.length > 0 &&
+                <IngredientsList
+                    ingredients={ingredients}
+                    toggleRecipeShown={toggleRecipeShown}
+                />
+            }
+
             {recipeShown && <ClaudeRecipe />}
         </main>
     )
