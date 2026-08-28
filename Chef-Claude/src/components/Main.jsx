b0c8f4017b6e4644cc675d0e578d2d345ff8dff8
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-28 17:53:21 +0530

    feat: implement smooth scrolling to recipe section upon recipe generation

--- Chef-Claude/src/components/Main.jsx
+++ Chef-Claude/src/components/Main.jsx
@@ -5,21 +5,37 @@ import {getRecipeFromGemini} from "/ai"
 
 export default function Main() {
     const [ingredients, setIngredients] = React.useState(
-        []
+        ["chicken", "all the main spices", "corn", "heavy cream", "pasta"]
     )
     const [recipe, setRecipe] = React.useState("")
+    const recipeSection = React.useRef(null)
+    
+    /**
+     * Challenge:
+     * Add a new effect that calls `recipeSection.current.scrollIntoView()`
+     * only if recipe is not an empty string and recipeSection.current is not null.
+     * Think carefully about what value(s) you would want to include in
+     * the dependencies array.
+     */
+
+    React.useEffect(() => {
+    if (recipe && recipeSection.current) {
+            recipeSection.current.scrollIntoView({behavior: "smooth"})
+        }
+    }, [recipe])
 
     async function getRecipe() {
         const recipeMarkdown = await getRecipeFromGemini(ingredients)
-        console.log(recipeMarkdown)
         setRecipe(recipeMarkdown)
     }
+
+    
 
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
-
+    
     return (
         <main>
             <form action={addIngredient} className="add-ingredient-form">
@@ -34,6 +50,7 @@ export default function Main() {
 
             {ingredients.length > 0 &&
                 <IngredientsList
+                    ref={recipeSection}
                     ingredients={ingredients}
                     getRecipe={getRecipe}
                 />
