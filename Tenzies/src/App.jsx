49c23a3164929b19c6dbd9fa68951488b6c8443c
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-30 18:35:27 +0530

    feat: enhance accessibility with sr-only message for game win; refactor gameWon logic and button text formatting

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -6,7 +6,8 @@ import Confetti from 'react-confetti'
 export default function App() {
     const [dice, setDice] = useState(() => generateAllNewDice())
 
-    let gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
+    const gameWon = dice.every(die => die.isHeld) &&
+        dice.every(die => die.value === dice[0].value)
 
     function generateAllNewDice() {
         return new Array(10)
@@ -17,7 +18,7 @@ export default function App() {
                 id: nanoid()
             }))
     }
-
+    
     function rollDice() {
         if (!gameWon) {
             setDice(oldDice => oldDice.map(die =>
@@ -50,13 +51,16 @@ export default function App() {
     return (
         <main>
             {gameWon && <Confetti />}
+            <div aria-live="polite" className="sr-only">
+                {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+            </div>
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
             <button className="roll-dice" onClick={rollDice}>
-                {gameWon ? 'New Game': 'Roll'}
+                {gameWon ? "New Game" : "Roll"}
             </button>
         </main>
     )
