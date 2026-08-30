820654108e498c8f6287cbcf0bbddbdbbe1e3c0b
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-30 15:16:09 +0530

    feat: update font family to Karla and improve button styles in CSS; refactor dice generation logic in App component

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -2,30 +2,29 @@ import Die from './components/Die'
 import { useState } from 'react'
 
 export default function App() {
-    const [dice , setDice] = useState(generateAllNewDice)
-
     
-
+    const [dice, setDice] = useState(generateAllNewDice())
+    
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
             .map(() => Math.ceil(Math.random() * 6))
     }
     
+    function rollDice() {
+        setDice(generateAllNewDice())
+    }
     
-    const diceElements = dice.map(num => 
-       <Die value={num}/>
-    )
+    const diceElements = dice.map(num => <Die value={num} />)
     
-
     return (
         <main>
-            
             <div className="dice-container">
-                {
-                    diceElements
-                }
+                {diceElements}
             </div>
+            
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            
         </main>
     )
 }
