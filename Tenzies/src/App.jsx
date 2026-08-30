8f415724ee0d073bc71dc1ebdb509be860e7365b
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-30 15:01:05 +0530

    feat: refactor app structure by removing Main component, adding Die component, and implementing dice generation logic

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,7 +1,31 @@
-import Main from "./components/main"
+import Die from './components/Die'
+import { useState } from 'react'
 
-export default function App(){
-  return (
-    <Main />
-  )
+export default function App() {
+    const [dice , setDice] = useState(generateAllNewDice)
+
+    
+
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
+    
+    
+    const diceElements = dice.map(num => 
+       <Die value={num}/>
+    )
+    
+
+    return (
+        <main>
+            
+            <div className="dice-container">
+                {
+                    diceElements
+                }
+            </div>
+        </main>
+    )
 }
