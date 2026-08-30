9b942510746b3c96522ea1d2627d2015e242cedb
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-08-30 15:25:37 +0530

    feat: refactor dice generation logic to include id and isHeld properties for each die

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,30 +1,32 @@
 import Die from './components/Die'
 import { useState } from 'react'
+import { nanoid } from "nanoid"
 
 export default function App() {
-    
     const [dice, setDice] = useState(generateAllNewDice())
-    
+
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
-            .map(() => Math.ceil(Math.random() * 6))
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            }))
     }
-    
+
     function rollDice() {
         setDice(generateAllNewDice())
     }
-    
-    const diceElements = dice.map(num => <Die value={num} />)
-    
+
+    const diceElements = dice.map(dieObj => <Die key={dieObj.id} value={dieObj.value} />)
+
     return (
         <main>
             <div className="dice-container">
                 {diceElements}
             </div>
-            
             <button className="roll-dice" onClick={rollDice}>Roll</button>
-            
         </main>
     )
 }
