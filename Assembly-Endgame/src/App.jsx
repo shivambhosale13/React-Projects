ca6214286bd2f12b551f8b30c69984050da870fc
shivambhosale13 <bhosaleshivam13@gmail.com>
2026-09-06 13:11:16 +0530

    feat: implement letter guessing functionality and update keyboard interaction

--- Assembly-Endgame/src/App.jsx
+++ Assembly-Endgame/src/App.jsx
@@ -3,8 +3,18 @@ import { languages } from "../languages"
 
 export default function AssemblyEndgame() {
     const [currentWord, setCurrentWord] = useState("react")
-    
+    const [guessedLetters, setGuessedLetters] = useState([])
+    console.log(guessedLetters)
+
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    function addGuessedLetter(letter) {
+        setGuessedLetters(prevLetters => 
+            prevLetters.includes(letter) ? 
+                prevLetters : 
+                [...prevLetters, letter]
+        )
+    }
 
     const languageElements = languages.map(lang => {
         const styles = {
@@ -21,13 +31,18 @@ export default function AssemblyEndgame() {
             </span>
         )
     })
-    
+
     const letterElements = currentWord.split("").map((letter, index) => (
         <span key={index}>{letter.toUpperCase()}</span>
     ))
-    
+
     const keyboardElements = alphabet.split("").map(letter => (
-        <button key={letter}>{letter.toUpperCase()}</button>
+        <button
+            key={letter}
+            onClick={() => addGuessedLetter(letter)}
+        >
+            {letter.toUpperCase()}
+        </button>
     ))
 
     return (
